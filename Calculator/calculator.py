1388e7f39a1ef048fe564f80587b055cd3fc6cc5
metebar <metebrtc@gmail.com>
2025-11-05 16:16:48 +0300

    Calculator

---
+++ Calculator/calculator.py
@@ -0,0 +1,65 @@
+import os
+logo = """
+ _____________________
+|  _________________  |
+| |              0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+operations = {"+" : add,
+              "-" : subtract,
+              "*" : multiply,
+              "/" : divide,          
+}
+
+print(logo)
+def calculator():
+    
+    num1 = float(input("What's the first number?: "))
+    
+    for operation in operations:
+        print(operation)
+        
+    keep_continue = True
+    
+    while keep_continue == True:
+        operation_symbol = input("Pick an operation.: ")
+        num2 = float(input("What's the second number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+          
+        confirmation = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+        if confirmation == "y":
+            num1 = answer
+            
+        elif confirmation == "n":
+            os.system('cls')
+            return calculator()
+    
+        else:
+            keep_continue = False
+        
+calculator()
